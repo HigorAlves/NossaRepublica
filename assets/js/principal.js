8b971d4e5540c50d51b043740760d369c1645aa4
Higor Alves <goodyearalves@gmail.com>
2018-06-06 23:13:15 -0300

    Melhoria no sistema de contas

--- assets/js/principal.js
+++ assets/js/principal.js
@@ -2,14 +2,16 @@ window.onload = function(){
     criarMoradores();
     criarMoradores();
 }
-
+var moradores = [];
 var qtmoradores = document.querySelector("#qtMoradores");
+var botao = document.querySelector("#calcular");
+
 qtmoradores.addEventListener("change",criaLista);
+botao.addEventListener("click", calculaDivisao);
 
 function criaLista(){
     document.querySelectorAll("#DS").forEach(e => e.parentNode.removeChild(e));
     for (i = 1; i <= qtmoradores.value; i++){
-        
         criarMoradores();
     }
 }
@@ -18,19 +20,20 @@ function criarMoradores(){
     var morador = document.querySelector("#morador");
 
     var divSupremo = document.createElement("div");
-    divSupremo.classList.add("col");
+    //divSupremo.classList.add("col");
     divSupremo.classList.add("col-md-6");
-    divSupremo.classList.add("col-sm-3");
+    divSupremo.classList.add("col-sm-12");
+    divSupremo.classList.add("col-lg-6");
     divSupremo.setAttribute("id", "DS");
 
     //Formulario para um morador
     var form = document.createElement("form");
     form.classList.add("formBox");
-    
+
     //Nome do morador
     var divFormMoradorPorcentagem = document.createElement("div");
     divFormMoradorPorcentagem.classList.add("form-row");
-    
+
     var divNomeMorador = document.createElement("div");
     divNomeMorador.classList.add("form-group");
     divNomeMorador.classList.add("col-md-6");
@@ -38,7 +41,7 @@ function criarMoradores(){
     var labelNomeMorador = document.createElement("label");
     labelNomeMorador.setAttribute("for", "inputNomeMorador");
     labelNomeMorador.innerHTML = "Nome do Morador";
-    
+
     var inputNomeMorador = document.createElement("input");
     inputNomeMorador.setAttribute("type", "name");
     inputNomeMorador.classList.add("form-control");
@@ -62,59 +65,6 @@ function criarMoradores(){
     inputPorcetagem.setAttribute("placeholder", "10");
     inputPorcetagem.setAttribute("pattern", "[1,]{1-3}");
 
-    var divPagamentos = document.createElement("div");
-    divPagamentos.classList.add("form-row");
-
-    //Agua
-    var divAgua = document.createElement("div");
-    divAgua.classList.add("form-group");
-    divAgua.classList.add("col");
-
-    var labelAgua = document.createElement("label");
-    labelAgua.setAttribute("for", "inputAgua");
-    labelAgua.innerHTML = "Agua";
-
-    var inputAgua = document.createElement("input");
-    inputAgua.setAttribute("type", "value");
-    inputAgua.classList.add("form-control");
-    inputAgua.setAttribute("id", "inputAgua");
-    inputAgua.setAttribute("placeholder", "72,5");
-    inputAgua.setAttribute("pattern", "[1,]{1-6}");
-
-    //Luz
-    var divLuz = document.createElement("div");
-    divLuz.classList.add("form-group");
-    divLuz.classList.add("col");
-
-    var labelLuz = document.createElement("label");
-    labelLuz.setAttribute("for", "inputLuz");
-    labelLuz.innerHTML = "Luz";
-
-    var inputLuz = document.createElement("input");
-    inputLuz.setAttribute("type", "value");
-    inputLuz.classList.add("form-control");
-    inputLuz.setAttribute("id", "inputLuz");
-    inputLuz.setAttribute("placeholder", "72,5");
-    inputLuz.setAttribute("pattern", "[1,]{1-6}");
-
-    //Internet
-    var divInternet = document.createElement("div");
-    divInternet.classList.add("form-group");
-    divInternet.classList.add("col");
-
-    var labelInternet = document.createElement("label");
-    labelInternet.setAttribute("for", "inputInternet");
-    labelInternet.innerHTML = "Internet";
-
-    var inputInternet = document.createElement("input");
-    inputInternet.setAttribute("type", "value");
-    inputInternet.classList.add("form-control");
-    inputInternet.setAttribute("id", "inputInternet");
-    inputInternet.setAttribute("placeholder", "72,5");
-    inputInternet.setAttribute("pattern", "[1,]{1-6}");
-
-    
-    
     morador.appendChild(divSupremo);
     divSupremo.appendChild(form);
     form.appendChild(divFormMoradorPorcentagem);
@@ -124,24 +74,43 @@ function criarMoradores(){
     divFormMoradorPorcentagem.appendChild(divPorcentagem);
     divPorcentagem.appendChild(labelPorcentagem);
     divPorcentagem.appendChild(inputPorcetagem);
-    form.appendChild(divPagamentos);
-    divPagamentos.appendChild(divAgua);
-    divAgua.appendChild(labelAgua);
-    divAgua.appendChild(inputAgua);
-    divPagamentos.appendChild(divLuz);
-    divLuz.appendChild(labelLuz);
-    divLuz.appendChild(inputLuz);
-    divPagamentos.appendChild(divInternet);
-    divInternet.appendChild(labelInternet);
-    divInternet.appendChild(inputInternet);
-    
-
 }
 
-function calculaDivisão(){
+function calculaDivisao(){
+  var aluguel = document.querySelector("#inputAluguel").value;
+  var iptu = document.querySelector("#inputIPTU").value;
+  var pessoas = document.querySelectorAll("#DS");
+  var IPTU = iptu/qtMoradores.value;
 
+  pessoas.forEach(function(pessoa){
+    var nome = pessoa.querySelector("#inputNomeMorador").value;
+    var agua = pessoa.querySelector("#inputAgua").value;
+    var luz = pessoa.querySelector("#inputLuz").value;
+    var internet = pessoa.querySelector("#inputInternet").value;
+
+    if (pessoa.querySelector("#inputPorAluguel").value > 0){
+      //Calcular porcentagem de cada morador
+    } else {
+      pMoradores = {
+        nome: nome,
+        agua: agua,
+        luz: luz,
+        internet: internet,
+        aluguel: (aluguel/qtMoradores.value),
+        iptu: IPTU
+      }
+    }
+    moradores.push(pMoradores);
+  });
+
+  resultado();
 }
 
 function resultado(){
-    //
+  var resultado = document.querySelector("#resultado");
+
+  moradores.forEach(function(morador){
+    resultado.innerHTML += morador.nome;
+    resultado.innerHTML += morador.agua;
+  });
 }
